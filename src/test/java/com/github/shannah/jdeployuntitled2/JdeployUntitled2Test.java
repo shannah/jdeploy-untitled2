ac15bc8bdb581ffed28cbce924689560981f9a48
steve <steve@host.docker.internal>
2023-12-27 15:42:00 -0800

    Commit message goes here

---
+++ src/test/java/com/github/shannah/jdeployuntitled2/JdeployUntitled2Test.java
@@ -0,0 +1,38 @@
+package com.github.shannah.jdeployuntitled2;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class JdeployUntitled2Test
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public JdeployUntitled2Test( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( JdeployUntitled2Test.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
